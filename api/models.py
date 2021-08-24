4e985aaecfc1e36e8ac60bee7ecd691ef38a3f02
Joshua Rodriguez <jr2444146@gmail.com>
2021-08-24 04:33:48 -0300

    heroku

--- api/models.py
+++ api/models.py
@@ -11,7 +11,7 @@ class Usuarios(AbstractBaseUser):
     nombre= models.CharField('nombre', blank=True, max_length=50, null=True)
     username= models.CharField('nombre de usuario', unique=True, max_length=50, null=True)
     password= models.CharField('Contraseña', null=True, max_length=50)
-    perfil= models.ImageField('imagen de perfil', upload_to='img/', height_field=None, width_field=None,max_length=200 )
+    perfil= models.ImageField('imagen de perfil', upload_to='static/img/', height_field=None, width_field=None,max_length=200 )
     telefono= models.CharField('numero telefonico', max_length=20, null=True)
     email= models.EmailField('correo de usuario', blank=True, max_length=50, null=True )
     linkedin= models.CharField('linkedin', unique=True, max_length=200)
