fe4499d34196256ee081926af60fd55677ab2123
Joshua Rodriguez <jr2444146@gmail.com>
2021-08-25 02:54:35 -0300

    cosas

--- back/settings.py
+++ back/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import dj_database_url 
 from decouple import config
 import django_heroku
-
+ 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
